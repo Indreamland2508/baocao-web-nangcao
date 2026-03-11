53e3b3b698fb63c752c5c87bd45753622454af08
Admin <Admin@NGUYENHOANGCHAU>
2026-03-11 07:58:07 +0700

    sepay success

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,41 @@
+﻿using BAOCAOWEBNANGCAO.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BAOCAOWEBNANGCAO.Controllers
+{
+    [ApiController]
+    [Route("api/payment")]
+    public class PaymentController : Controller
+    {
+        private readonly CampingDbContext _context;
+
+        public PaymentController(CampingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("webhook")]
+        public async Task<IActionResult> Webhook([FromBody] WebhookModel data)
+        {
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(x => x.Id == data.orderId);
+
+            if (order == null)
+                return NotFound();
+
+            order.Status = "Paid";
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+
+    public class WebhookModel
+    {
+        public int orderId { get; set; }
+        public decimal amount { get; set; }
+        public string status { get; set; }
+    }
+}
